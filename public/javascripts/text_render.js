ba19c4ad816c8fc5d342c109ca2f7737827dc36a
App Academy Student <student@appacademy.io>
2015-03-10 23:03:18 -0700

    still trying to fix this but not sure whether this is the right path

--- public/javascripts/text_render.js
+++ public/javascripts/text_render.js
@@ -4,18 +4,17 @@ var three_is_loaded = false
 self.addEventListener('message', function(msg) {
   if (!three_is_loaded) {
     importScripts(msg.data.three, msg.data.helv)
-    // renderer = new THREE.WebGLRenderer( { antialias: true } );
     three_is_loaded = true
   }
   var text = msg.data.text
   var text_array = text.split( "\n" )
-  var group = new THREE.Group()
-  for (var i = 0; i < text_array.length; i++)
-  {
+  var monkey_pieces = []
+  for (var i = 0; i < text_array.length; i++) {
     fmp = flying_monkey_piece(text_array[i], i, msg.data.jk)
-    group.add(fmp)
+    monkey_pieces.push([fmp.geometries[1].toJSON(), fmp.materials[1].toJSON())])
   }
-  self.postMessage({'group': group.toJSON()})
+
+  self.postMessage({'monkey_pieces': monkey_pieces})
 })
 
 
@@ -49,4 +48,52 @@ function flying_monkey_piece(text, offset, jk) {
   	textMesh1 = new THREE.Mesh( textGeo, material );
     textMesh1.position.setY( - jk.textSize * offset * 1.5 )
     return textMesh1
+  }
+
+
+
+  saveGeometryToJSON = function (geometry) {
+      var i,
+          json = {
+              metadata: {
+                  formatVersion: 3
+              },
+              scale: 1.000000,
+              materials: [],
+              vertices: [],
+              morphTargets: [],
+              morphColors: [],
+              normals: [],
+              colors: [],
+              uvs: [[]],
+              faces: []
+          };
+
+      for (i = 0; i < geometry.vertices.length; i++) {
+          json.vertices.push(geometry.vertices[i].position.x);
+          json.vertices.push(geometry.vertices[i].position.y);
+          json.vertices.push(geometry.vertices[i].position.z);
+      }
+
+      for (i = 0; i < geometry.faces.length; i++) {
+          if (geometry.faces[i].d) {
+              json.faces.push(1);
+          } else {
+              json.faces.push(0);
+          }
+
+          json.faces.push(geometry.faces[i].a);
+          json.faces.push(geometry.faces[i].b);
+          json.faces.push(geometry.faces[i].c);
+
+          if (geometry.faces[i].d) {
+              json.faces.push(geometry.faces[i].d);
+          }
+
+          json.normals.push(geometry.faces[i].normal.x);
+          json.normals.push(geometry.faces[i].normal.y);
+          json.normals.push(geometry.faces[i].normal.z);
+      }
+
+      return JSON.stringify(json, null, '<br>');
   }
