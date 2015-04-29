087d2a89b574d74e86a8da3cab2421dc6d77c20b
JoshuaSP <joshua.s.penman@gmail.com>
2015-04-29 04:47:40 -0700

    camera fustrum and default dolly-in

--- app/assets/javascripts/camera.js
+++ app/assets/javascripts/camera.js
@@ -1,5 +1,5 @@
 define(['three'], function (THREE) {
-  camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 25000 );
+  camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 100000 );
   camera.position.z = 0;
   return camera;
 });
--- app/assets/javascripts/controls.js
+++ app/assets/javascripts/controls.js
@@ -1,5 +1,6 @@
 define(['three', 'camera'], function(THREE, camera) {
   controls = new THREE.OrbitControls( camera );
   controls.damping = 0.2;
+  controls.dollyIn(2);
   return controls;
 });
