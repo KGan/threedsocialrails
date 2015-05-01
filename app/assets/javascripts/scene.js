1002ad7ee7363c1ea88e1aed6eedddaf9ab1aef0
JoshuaSP <joshua.s.penman@gmail.com>
2015-04-30 19:42:02 -0700

    gradiant background

--- app/assets/javascripts/scene.js
+++ app/assets/javascripts/scene.js
@@ -1,5 +1,5 @@
 define(['three'], function (THREE) {
   var scene = new THREE.Scene();
-  scene.fog = new THREE.Fog( 0x9daaaa, 0.002, 100000);
+  // scene.fog = new THREE.Fog( 0x9daaaa, 0.002, 100000);
   return scene;
 });
