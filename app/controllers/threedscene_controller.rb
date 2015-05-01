e9ba7dc80defd6b0585da9200423216282a3997a
Kevin Gan <lss.kevin.gan@gmail.com>
2015-05-01 13:32:14 -0700

    figaro in our controllers

--- app/controllers/threedscene_controller.rb
+++ app/controllers/threedscene_controller.rb
@@ -1,10 +1,10 @@
 class ThreedsceneController < ApplicationController
   def index
     client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = "0X4UNFV7P3RzUDWlly7fgD1Cx"
-        config.consumer_secret     = "3QSn43Vl505x1TbClrfEz7JciF6dOtrHhBH0X9CwfX6Ofzby6u"
-        config.access_token        = "2703413065-eoy3gCO8LSKdGizpGRW4nkiPRgv1FK4OWdPQfr4"
-        config.access_token_secret = "zNXDFeQZrm3uEkMabpD5KyTYPTqdMn65iGT2eJixU8omP"
+        config.consumer_key        = Figaro.env.twitter_consumer_key
+        config.consumer_secret     = Figaro.env.twitter_consumer_secret
+        config.access_token        = Figaro.env.twitter_access_token
+        config.access_token_secret = Figaro.env.twitter_access_secret
     end
 
     @trends = client.trends(1).map{ |trend| trend.name.gsub('#', '') }.join(', ')
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -12,10 +12,10 @@ class TweetsController < WebsocketRails::BaseController
     controller_store[:uids] << channel_id
     connection_store[:thread] = Thread.new(channel_id, message["tags"]) do |cid, tags|
       Thread.current[:client] = Twitter::Streaming::Client.new do |config|
-        config.consumer_key        = "0X4UNFV7P3RzUDWlly7fgD1Cx"
-        config.consumer_secret     = "3QSn43Vl505x1TbClrfEz7JciF6dOtrHhBH0X9CwfX6Ofzby6u"
-        config.access_token        = "2703413065-eoy3gCO8LSKdGizpGRW4nkiPRgv1FK4OWdPQfr4"
-        config.access_token_secret = "zNXDFeQZrm3uEkMabpD5KyTYPTqdMn65iGT2eJixU8omP"
+        config.consumer_key        = Figaro.env.twitter_consumer_key
+        config.consumer_secret     = Figaro.env.twitter_consumer_secret
+        config.access_token        = Figaro.env.twitter_access_token
+        config.access_token_secret = Figaro.env.twitter_access_secret
       end
 
       Thread.current[:client].filter(track: tags) do |object|
@@ -34,10 +34,10 @@ class TweetsController < WebsocketRails::BaseController
 
   def fetch_fifty(query)
     client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = "0X4UNFV7P3RzUDWlly7fgD1Cx"
-        config.consumer_secret     = "3QSn43Vl505x1TbClrfEz7JciF6dOtrHhBH0X9CwfX6Ofzby6u"
-        config.access_token        = "2703413065-eoy3gCO8LSKdGizpGRW4nkiPRgv1FK4OWdPQfr4"
-        config.access_token_secret = "zNXDFeQZrm3uEkMabpD5KyTYPTqdMn65iGT2eJixU8omP"
+        config.consumer_key        = Figaro.env.twitter_consumer_key
+        config.consumer_secret     = Figaro.env.twitter_consumer_secret
+        config.access_token        = Figaro.env.twitter_access_token
+        config.access_token_secret = Figaro.env.twitter_access_secret
     end
     query_terms = query.split(',').map(&:strip)
     results_per_term = 50 / query_terms.length
